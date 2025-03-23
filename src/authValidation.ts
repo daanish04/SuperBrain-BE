840a4c49860ce6ab066ead1802487c76021a4ded
daanish04 <108368229+daanish04@users.noreply.github.com>
2025-03-24 02:09:00 +0530

    Added alerts for Signup auth validation

--- src/authValidation.ts
+++ src/authValidation.ts
@@ -9,9 +9,9 @@ export const authSchema = z.object({
     .string()
     .min(8, "Password must be at least 8 characters long")
     .max(20, "Password must be at most 20 characters long")
-    .regex(/[A-Z]/, "Must contain at least one uppercase letter")
-    .regex(/[a-z]/, "Must contain at least one lowercase letter")
-    .regex(/\d/, "Must contain at least one number")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/\d/, "Password must contain at least one number")
     .regex(
       /[@$!%*?&]/,
       "Must contain at least one special character (@$!%*?&)"
